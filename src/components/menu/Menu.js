ebbcb05e9575f285b2ea9fa5731b35f1f23e772d
Radosław Łodziński <radoslawlodzinski@Radoslaws-MacBook-Pro.local>
2020-12-03 16:14:27 +0100

    notes input bug

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table, Button, Form } from 'react-bootstrap';
 import { FiShoppingCart } from 'react-icons/fi';
 import { ImCross } from 'react-icons/im';
 import './Menu.css'
+import Popup from 'reactjs-popup';
+import "reactjs-popup/dist/index.css";
 
 
 const menuColumns = [
@@ -38,6 +40,8 @@ const cartColumns = [
         text: "Cena",
     },
 ];
+
+const tables =["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 function Menu() {
@@ -80,61 +84,147 @@ function Menu() {
             price: "31 zł"
         }
     ])
-
     const [shoppingCart, setShoppingCart] = useState([])
+    const [popupOpen, setPopupOpen] = useState(false)
+    const [name, setName] = useState('')
+    const [tableNumer, setTableNumber] = useState(tables[0])
+    const [orderValue, setOrderValue] = useState()
+    const [orderNotes, setOrderNotes] = useState([])
 
+
+    // useEffect(() => {
+    //     fillNotes()
+    // }, [])
+
+
+    const orderButtonClick = () => {
+        console.log(name)
+        console.log(tableNumer)
+        sendOrder()
+        setPopupOpen(false)
+    }
+
+    const sendOrder = () => {
+        //POST to database
+    }
+
+    const calcOrderValue = () => {
+        var value = 0
+        shoppingCart.forEach(item => {
+            var price = item.price.split(" ")
+            value += parseInt(price[0])
+        })
+        setOrderValue(value)
+    }
 
     return (
-        <div className="container">
-            <div className="menu-container"> 
-                <Table bordered hover >
-                    <thead>
-                        <tr>
-                            {menuColumns.map(column => <td>{column.text}</td>)}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {menuItems.map((item, index) => (
-                            <tr>
-                                {menuColumns.map(column => <td>{item[column.dataField]}</td>)}
-                                <td><Button variant="dark" onClick={() => {
-                                    setShoppingCart([...shoppingCart, menuItems[index]])
-                                    console.log(shoppingCart)
-                                }}><FiShoppingCart/></Button></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
-            <div className="cart-container">
-                <div className="cart">
+        <div className="all-container">
+            <div className="container">
+                <div className="menu-container"> 
                     <Table bordered hover >
                         <thead>
                             <tr>
-                                {cartColumns.map(column => <td>{column.text}</td>)}
+                                {menuColumns.map(column => <td>{column.text}</td>)}
                             </tr>
                         </thead>
                         <tbody>
-                            {shoppingCart.map((item, index) => (
+                            {menuItems.map((item, index) => (
                                 <tr>
-                                    {cartColumns.map(column => <td>{item[column.dataField]}</td>)}
-                                    <td>
-                                        <Form.Control as="textarea" rows={3} />
-                                    </td>
-                                    <td>
-                                        <Button variant="danger" onClick={() => {
-                                            setShoppingCart(shoppingCart.filter(item => item !== shoppingCart[index])) //(BUG)deletes all same items
-                                        }}>
-                                            <ImCross/>
-                                        </Button>
-                                    </td>
+                                    {menuColumns.map(column => <td>{item[column.dataField]}</td>)}
+                                    <td><Button variant="dark" onClick={() => {
+                                        setShoppingCart([...shoppingCart, menuItems[index]])
+                                        console.log(shoppingCart)
+                                    }}><FiShoppingCart/></Button></td>
                                 </tr>
                             ))}
                         </tbody>
                     </Table>
                 </div>
-                <Button>Złóż zamówienie</Button>
+                <div className="cart-container">
+                    <div className="cart">
+                        <Table bordered hover >
+                            {/* <thead>
+                                <tr>
+                                    {cartColumns.map(column => <td>{column.text}</td>)}
+                                </tr>
+                            </thead> */}
+                            <tbody>
+                                {shoppingCart.map((item, index) => (
+                                    <tr>
+                                        {cartColumns.map(column => <td>{item[column.dataField]}</td>)}
+                                        <td>
+                                            <Form.Control 
+                                            as="textarea" 
+                                            rows={2} 
+                                            value={orderNotes[index]} 
+                                            onChange={({target:{value}}) => {
+                                                let newArr = orderNotes
+                                                console.log(orderNotes)
+                                                newArr[index] = value
+                                                setTableNumber(newArr)
+                                                console.log(orderNotes)
+                                            }}/>
+                                        </td>
+                                        <td>
+                                            <Button variant="danger" onClick={() => {
+                                                setShoppingCart(shoppingCart.filter(item => item !== shoppingCart[index])) //(BUG)deletes all same items
+                                                setOrderNotes(orderNotes.filter(item => item !== orderNotes[index])) //(BUG)deletes all same items
+                                            }}>
+                                                <ImCross/>
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                    <Button onClick={() => {
+                        calcOrderValue()
+                        setPopupOpen(true)
+                        }}
+                    >
+                        Złóż zamówienie
+                    </Button>
+                </div>
             </div>
+            
+                <Popup
+                modal
+                open={popupOpen}
+                onClose={() => setPopupOpen(false)}
+                contentStyle={{ width: "488px" }}
+                >
+                    <div className="order-popup">
+                        <Table bordered hover>
+                                <tbody>
+                                    {shoppingCart.map((item, index) => (
+                                        <tr>
+                                            {cartColumns.map(column => <td>{item[column.dataField]}</td>)}
+                                            <td>
+                                                <Form.Text as="text">{orderNotes[index]}</Form.Text>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            <Form.Text as="text">Suma: {orderValue} zł</Form.Text>
+                        <div>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Imię</Form.Label>
+                                <Form.Control placeholder="Imię" value={name} onChange={({target:{value}}) => setName(value)}/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Numer stolika</Form.Label>
+                                <Form.Control 
+                                as="select" value={tableNumer} onChange={({target:{value}}) => setTableNumber(value)}
+                                >
+                                    {tables.map(item => <option>{item}</option>)}
+                                </Form.Control>
+                            </Form.Group>
+                            <Button onClick={orderButtonClick}>Zamów</Button>
+                        </div>
+                    </div>
+                </Popup>
         </div>
     )
 }
